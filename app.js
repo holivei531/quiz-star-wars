f7750380206cde4b7652fc275db47363aeb0878a
Juniork Oliveira <juniorkoliveira@hotmail.com>
2021-02-16 14:50:07 -0300

    modifying functionality

--- app.js
+++ app.js
@@ -1,72 +1,46 @@
-// // Obtendo referência dos elementos
-// const form = document.querySelector('.quiz-form');
-// const finalResult = document.querySelector('.result');
-
-// // Armazanando as respostas corretas do quiz
-// const correctAnswers = ['A', 'A', 'A', 'A'];
-
-// // Adicionando evento de envio ao form
-// form.addEventListener('submit', event => {
-//     event.preventDefault();
-
-//     // Adicionando uma pontuação baseada nas respostas do usuário
-//     let score = 0; 
-//     // Adicionando valores checados do input
-//     const userAnswers = [
-//         form.inputQuestion1.value,
-//         form.inputQuestion2.value,
-//         form.inputQuestion3.value,
-//         form.inputQuestion4.value,
-//     ];
-//     // Checando respostas
-//     userAnswers.forEach((userAnswer, index) => {
-//         if (userAnswer === correctAnswers[index]) {
-//             score += 25;
-//         };
-//     });
-
-//     // Rolando a página para o top após enviar o form (para melhor visibilidade no mobile)
-//     scrollTo(0, 0);
-
-//     // Removendo a classe display none
-//     finalResult.classList.remove('d-none');
-
-//     // Mostrando a porcentagem de acerto do usuário
-//     let counter = 0;
-
-//     const timer = setInterval(() => {
-//         if (counter === score) {
-//             clearInterval(timer);
-//         };
-//         finalResult.querySelector('span').textContent = `${counter}%`;
-//         counter++;
-//     }, 10);
-// });
-
-// obtendo referencia dos elementos
+// Obtendo referência dos elementos
 const form = document.querySelector('.quiz-form');
+const finalResult = document.querySelector('.result');
 
-// armazenando respostas corretas
+// Armazanando as respostas corretas do quiz
 const correctAnswers = ['A', 'A', 'A', 'A'];
 
-// adicionando evento de envio no formulario
+// Adicionando evento de envio ao form
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-    let score = 0;
-
-    // obtendo resposta do usuario
+    // Adicionando uma pontuação baseada nas respostas do usuário
+    let score = 0; 
+    // Adicionando valores checados do input
     const userAnswers = [
         form.inputQuestion1.value,
         form.inputQuestion2.value,
         form.inputQuestion3.value,
-        form.inputQuestion4.value
+        form.inputQuestion4.value,
     ];
-
-    // checando respostas
+    // Checando respostas
     userAnswers.forEach((userAnswer, index) => {
         if (userAnswer === correctAnswers[index]) {
-            console.log(score += 25);
+            score += 25;
         };
     });
+
+    // Rolando a página para o top após enviar o form (para melhor visibilidade no mobile)
+    scrollTo(0, 0);
+
+    // Removendo a classe display none
+    finalResult.classList.remove('d-none');
+
+    // Mostrando a porcentagem de acerto do usuário
+    let counter = 0;
+
+    const timer = setInterval(() => {
+        if (counter === score) {
+            clearInterval(timer);
+        };
+        finalResult.querySelector('span').textContent = `${counter}%`;
+        counter++;
+    }, 10);
 });
+
+    
